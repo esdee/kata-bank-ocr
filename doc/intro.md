45a2439f287a9001d437a889e208686c546506cc
Shashy Dass <ShashyD@gmail.com>
2014-02-28 10:59:47 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kata-bank-ocr
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/kata_bank_ocr/core_test.clj
@@ -0,0 +1,7 @@
+(ns kata-bank-ocr.core-test
+  (:require [clojure.test :refer :all]
+            [kata-bank-ocr.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
